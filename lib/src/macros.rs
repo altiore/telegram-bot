98bd717809b3f0ca3fdfc9c5efdab7e6711df9d7
Razzwan <razvanlomov@gmail.com>
2024-01-26 23:58:02 +0300

    feat(login_url): added reply_markup macros

--- lib/src/macros.rs
+++ lib/src/macros.rs
@@ -108,12 +108,26 @@ macro_rules! reply_markup {
         $crate::InlineKeyboardMarkup::from(vec![$(reply_markup![_inline_keyboard_row, $($content)*]), *])
     );
 
-    (login_url, $([$($content:tt)*]), *) => (
-        $crate::InlineKeyboardMarkup::from(vec![$(reply_markup![_inline_keyboard_row, $($content)*]), *])
+    (login_url, $text:tt, $url:tt) => (
+        // markup.add_row(vec![InlineKeyboardButton::login_url("Войти", LoginUrl {
+        //     url: String::from("https://google.com"),
+        //     forward_text: None,
+        //     bot_username: None,
+        //     request_write_access: None,
+        // })]);
+
+        $crate::InlineKeyboardMarkup::from(vec![
+            vec![$crate::InlineKeyboardButton::login_url($text, LoginUrl {
+                url: String::from($url),
+                forward_text: None,
+                bot_username: None,
+                request_write_access: None,
+            })]
+        ])
     );
 
     (_inline_keyboard_row, ($($acc:tt)*); $text:tt $request:tt $callback:tt) => (
-        vec![$($acc)* reply_markup!(_inline_keyboard_button, $request,  $text, $callback)]
+        vec![$($acc)* reply_markup!(_inline_keyboard_button, $request, $text, $callback)]
     );
     (_inline_keyboard_row, $($text:tt $request:tt $callback:tt), *) => (
         vec![$(reply_markup!(_inline_keyboard_button, $request, $text, $callback)), *]
@@ -260,7 +274,6 @@ mod tests {
             request_write_access: None,
         })]);
 
-        // TODO: write tests for login_url
-        // assert_eq!(markup, reply_markup!(login_url, []))
+        assert_eq!(markup, reply_markup!(login_url, "Войти", "https://google.com"));
     }
 }
