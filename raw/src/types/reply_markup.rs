574d968f2d1cf2ef5ddc93ff9d12033cdfe46197
Razzwan <razvanlomov@gmail.com>
2024-01-27 00:22:41 +0300

    fix(login_url): fix serialize

--- raw/src/types/reply_markup.rs
+++ raw/src/types/reply_markup.rs
@@ -305,8 +305,11 @@ pub enum InlineKeyboardButtonKind {
 #[derive(Debug, Clone, PartialEq, PartialOrd, Serialize)]
 pub struct LoginUrl {
     pub url: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub forward_text: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub bot_username: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub request_write_access: Option<bool>,
 }
 
